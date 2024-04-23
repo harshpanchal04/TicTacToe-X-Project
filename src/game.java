3cdc532bc06d871f2c3a96f76d5764ca566eadbd
Harsh Panchal <131860336+harshpanchal04@users.noreply.github.com>
2024-04-23 14:13:55 +0530

    Add files via upload
    Models of TicTacToe Game

---
+++ src/game.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public class game {
+    board board;
+    List<player> ListOfPlayers;
+    int nextPlayerMoveIndex;
+    player winner;
+    List<Move> ListOfMoves;
+    gameState gameState;
+
+    public static void main(String[] args) {
+
+    }
+}

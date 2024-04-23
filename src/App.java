935f8d49e4e4427907642910ad429ff8fe9912ff
Harsh Panchal <harshkumarpanchal04@gmail.com>
2024-04-24 01:39:45 +0530

    Welcoming player

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        
+        System.out.println("Welcome to the Game");
+        System.out.println("How do you like to play?");
     }
 }
